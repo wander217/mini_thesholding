d8ae4d4097e53bc4c43e7ccbe34773e7da55e2aa
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-05-06 00:35:10 +0700

    initial

--- measure/loss/heed_loss.py
+++ measure/loss/heed_loss.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 from typing import Dict
-from measure.loss.bce_loss import BceLoss
+from measure.loss.dice_loss import DiceLoss
 from collections import OrderedDict
 from torch import Tensor
 
@@ -8,7 +8,7 @@ from torch import Tensor
 class HeedLoss(nn.Module):
     def __init__(self, heed: Dict):
         super().__init__()
-        self._binaryLoss = BceLoss(**heed)
+        self._binaryLoss = DiceLoss(**heed)
 
     def __call__(self, pred: Tensor, batch: OrderedDict) -> Tensor:
         binaryDist: Tensor = self._binaryLoss(pred, batch['binaryMap'])
