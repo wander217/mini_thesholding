909be3e62ecdab81812c716b7e4fffb64c42c4db
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-05-05 23:59:10 +0700

    initial

---
+++ measure/loss/bce_loss.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+from torch import Tensor
+import torch.nn.functional as F
+import torch
+
+
+class BceLoss(nn.Module):
+    def __init__(self, ratio: float, eps: float):
+        super().__init__()
+        self._ratio: float = ratio
+        self._eps: float = eps
+
+    def __call__(self,
+                 pred: Tensor,
+                 probMap: Tensor,
+                 probMask: Tensor) -> Tensor:
+        pos: Tensor = probMap.byte()
+        neg: Tensor = (1 - probMap).byte()
+        loss: Tensor = F.binary_cross_entropy(pred.float(), probMap, reduction='none')[:, 0, :, :]
+
+        posNum: int = int(pos.float().sum())
+        posLoss: Tensor = loss * pos.float()
+        negNum: int = min(int(neg.sum()), int(posNum * self._ratio))
+        negLoss: Tensor = loss * neg.float()
+        negLoss, _ = torch.topk(negLoss.view(-1), negNum)
+        loss: Tensor = (posLoss.sum() + negLoss.sum()) / (posNum + negNum + self._eps)
+        return loss
--- measure/loss/heed_loss.py
+++ measure/loss/heed_loss.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 from typing import Dict
-from measure.loss.dice_loss import DiceLoss
+from measure.loss.bce_loss import BceLoss
 from collections import OrderedDict
 from torch import Tensor
 
@@ -8,7 +8,7 @@ from torch import Tensor
 class HeedLoss(nn.Module):
     def __init__(self, heed: Dict):
         super().__init__()
-        self._binaryLoss = DiceLoss(**heed)
+        self._binaryLoss = BceLoss(**heed)
 
     def __call__(self, pred: Tensor, batch: OrderedDict) -> Tensor:
         binaryDist: Tensor = self._binaryLoss(pred, batch['binaryMap'])
